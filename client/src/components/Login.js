dff812f6bb2acd6851b6ac57ae08e123538da4ca
bobbidigi <bobbidigi34@gmail.com>
2020-03-22 19:44:24 -0400

    add login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,38 @@
-import React from "react";
 import React, {useState} from "react";
 import api from '../utils/api'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [error, setError] = useState()
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  }) 
+
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    api()
+    .post('/api/login', data)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem("token", res.data.payload)
+      props.history.push('/bubbles')
+    })
+    .catch(err => {
+      setError(err.response.data.error)
+      console.log(err.response.data.error)
+    })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
